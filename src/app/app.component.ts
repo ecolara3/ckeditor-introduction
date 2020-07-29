4d409ed63b2a11d5c59d9bc3211974b11fec6e38
Robert Lara III <admin@robertlara.me>
2020-07-29 04:55:06 -0500

    Added Base Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
-
+import * as ClassicEditor from 'src/ckeditor/ckeditor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ckeditor';
+  public Editor = ClassicEditor;
+  public config: any;
+
+  constructor() {
+    this.config = {
+      toolbar: {
+        items: [
+          'bold',
+          'italic',
+          'link',
+          'bulletedList',
+          'numberedList',
+          'imageUpload',
+          'blockQuote',
+          'undo',
+          'redo'
+        ]
+      },
+      image: {
+        toolbar: [
+          'imageStyle:full',
+          'imageStyle:side',
+          '|',
+          'imageTextAlternative'
+        ]
+      },
+      simpleUpload: {
+        // The URL that the images are uploaded to.
+        uploadUrl: 'http://example.com',
+
+        // Enable the XMLHttpRequest.withCredentials property.
+        withCredentials: false,
+
+        // Headers sent along with the XMLHttpRequest to the upload server.
+        //headers: {
+        //  'X-CSRF-TOKEN': 'CSFR-Token',
+        //  Authorization: 'Bearer <JSON Web Token>'
+        //}
+      },
+      // This value must be kept in sync with the language defined in webpack.config.js.
+      language: 'en'
+    };
+
+    ClassicEditor.builtinPlugins.map(plugin => plugin.pluginName);
+  }
 }
